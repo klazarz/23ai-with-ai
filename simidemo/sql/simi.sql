7013388b7ad9a7566709da9587bc5e5e3e896003
Kevin Lazarz <kevin.lazarz@oracle.com>
2025-03-14 10:32:46 +0100

    update simi

--- simidemo/sql/simi.sql
+++ simidemo/sql/simi.sql
@@ -1,5 +1,20 @@
-drop table products_vector;
+--load model into DB
+begin
+   dbms_vector.drop_onnx_model(
+      model_name => 'all_MiniLM_L12_v2',
+      force      => true
+   );
+   dbms_vector.load_onnx_model(
+      directory  => 'DEMO_PY_DIR',
+      file_name  => 'all_MiniLM_L12_v2.onnx',
+      model_name => 'demo_model'
+   );
+end;
+/
 
+
+-- create vector
+drop table products_vector;
 
 create table products_vector
    as
@@ -17,7 +32,7 @@ create table products_vector
         from products p;
 
 
-
+--test query
 select p.prod_desc,
        p.prod_category_desc,
        p.prod_list_price
@@ -33,3 +48,40 @@ select p.prod_desc,
    cosine
 )
  fetch first 4 rows only;
+
+
+
+
+
+ -- show also the distance
+
+select p.prod_desc,
+       p.prod_category_desc,
+       p.prod_list_price,
+       vector_distance(
+          p.embedding,
+          dbms_vector_chain.utl_to_embedding(
+             'tennis',
+             json(
+                   '{"provider":"database", "model":"demo_model"}'
+                )
+          ),
+          cosine
+       ) as distance
+  from products_vector p
+ order by vector_distance(
+   p.embedding,
+   dbms_vector_chain.utl_to_embedding(
+      'tennis',
+      json(
+            '{"provider":"database", "model":"demo_model"}'
+         )
+   ),
+   cosine
+)
+ fetch first 4 rows only;
+
+
+
+select *
+  from products_vector;
